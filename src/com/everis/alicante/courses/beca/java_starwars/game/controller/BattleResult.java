6c119e59f1a64ba7c7eb3a03e22a853406c07d3e
franperiagoliver <franperiagoliver@gmail.com>
2017-07-25 14:53:07 +0200

    Subida de ficheros

---
+++ src/com/everis/alicante/courses/beca/java_starwars/game/controller/BattleResult.java
@@ -0,0 +1,11 @@
+package com.everis.alicante.courses.beca.java_starwars.game.controller;
+
+public class BattleResult {
+    
+	@Override
+    public String toString() {
+        return "Result of the Battle:";
+    }
+	
+	
+}
